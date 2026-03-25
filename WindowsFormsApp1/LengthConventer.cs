584cd2ba64b2cd6e39dceed61fab992d95282446
Andrej Katin <katin.andrej96@gmail.com>
2026-03-25 11:01:17 +0100

    Ovo je testna poruka.

--- WindowsFormsApp1/LengthConventer.cs
+++ WindowsFormsApp1/LengthConventer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
+    // Ovo je drugi komentar.
     public class LengthConventer : IConvert
     {
         public double Convert(double value)
--- WindowsFormsApp1/MassConventer.cs
+++ WindowsFormsApp1/MassConventer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
+    // Ovo je komentar
     public class MassConventer : IConvert
     {
         public double Convert(double value)
